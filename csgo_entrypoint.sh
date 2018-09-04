3c45c6a41409343f5585496a90b033db4efc9ba8
mathieuduperre <mathieu.duperre@gmail.com>
2018-09-04 13:45:08 -0400

    Update csgo_entrypoint.sh

--- csgo_entrypoint.sh
+++ csgo_entrypoint.sh
@@ -6,4 +6,5 @@ trap '' TERM INT HUP
 ./steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 740 validate +quit
 
 cd csgo
-./srcds_run -console -usercon +hostname "$CSGO_HOSTNAME" +net_public_adr "$PUBLIC_IP" +ip "$PRIVATE_IP" -port 27015 -game csgo -tickrate 100 +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust2 +maxplayers 22 -maxplayers_override 22 +exec server.cfg -stringtables -condebug +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN"
+./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN"
+#./srcds_run -console -usercon +hostname "$CSGO_HOSTNAME" +net_public_adr "$PUBLIC_IP" +ip "$PRIVATE_IP" -port 27015 -game csgo -tickrate 100 +game_type 0 +game_mode 0 +mapgroup mg_bomb +map de_dust2 +maxplayers 22 -maxplayers_override 22 +exec server.cfg -stringtables -condebug +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN"
