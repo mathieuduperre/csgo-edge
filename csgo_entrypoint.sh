5e7731325c4994d8f2658799500ce44326212c53
mathieuduperre <mathieu.duperre@gmail.com>
2019-01-29 09:04:07 -0500

    Update csgo_entrypoint.sh

--- csgo_entrypoint.sh
+++ csgo_entrypoint.sh
@@ -5,5 +5,5 @@ trap '' TERM INT HUP
 # Ensure csgo is up to date
 #./steamcmd.sh +login anonymous +force_install_dir ./csgo +app_update 740 validate +quit
 
-cd /root/csgo
+cd csgo
 ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount "$STEAM_ACCOUNT_TOKEN"
